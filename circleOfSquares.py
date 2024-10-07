ee88215ee4a8a5c34f875a740ebf4c4593cba875
Grumpy-Tomato <cormier.tanner@gmail.com>
2024-10-06 21:46:31 -0300

    - made a list of different square sizes - looped throught the list so there would be 4 sets of square circles

--- circleOfSquares.py
+++ circleOfSquares.py
@@ -3,16 +3,19 @@ import time
 
 shape('turtle')
 
-def square(size=100):
+def square(sideLength=100):
     for i in range(4):
-        forward(size)
+        forward(sideLength)
         right(90)
 
-def circleOfSquares():
+def circleOfSquares(squareSize=100):
     speed(0)
     for i in range(72):
-        square()
+        square(squareSize)
         right(5)
 
-circleOfSquares()
+sideLengthSizes = [65, 100, 150, 225]
+for i in sideLengthSizes:
+    circleOfSquares(i)
+
 time.sleep(5)
